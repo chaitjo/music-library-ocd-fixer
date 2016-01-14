de1ce9179d2b1e8c0da6eafad341ff243f0b8137
Chaitanya Joshi <ckjoshi9@gmail.com>
2016-01-14 20:02:07 +0800

    Added comments

--- fix.py
+++ fix.py
@@ -1,36 +1,40 @@
+"""
+Music Library OCD fixer by Chaitanya Joshi
+"""
+
 import os, sys, urllib2, eyed3
 
 def getData(name):
-	name = name.replace(' ', '+').replace('_', '+').lower()
-
+	"""
+	Method to make API queries using the name parameter, to fetch metadata.
+	"""
+	name = name.replace(' ', '+').replace('_', '+').lower()		#make name variable devoid of crap like [lyrics], [hd], etc.
 	track_name = urllib2.urlopen("http://rhythmsa.ga/api.php?api=track_name&q={0}".format(name))
 	artist_name = urllib2.urlopen("http://rhythmsa.ga/api.php?api=artist_name&q={0}".format(name))
 	album_name = urllib2.urlopen("http://rhythmsa.ga/api.php?api=album_name&q={0}".format(name))
-
 	return ( unicode(track_name.read(),'utf-8'), unicode(artist_name.read(),'utf-8'), unicode(album_name.read(),'utf-8') )
 
 def modify(folderPath):
+	"""
+	Method to update metadata and rename all mp3 files in folderPath.
+	"""
 	filesList = os.listdir(folderPath)
-	pathsList = {}
+	pathsList = {}		#a dictionary mapping the name of a file to its complete path
 	for file in filesList:
 	    pathsList.update( {file[:-4] : folderPath+file} ) 
 
 	for songName in pathsList.keys():
 		song = eyed3.load(pathsList[songName])
 		metadata = getData(songName)
-
 		try:
 			song.tag.title = metadata[0]
 			song.tag.artist = metadata[1]
 			song.tag.album = metadata[2]
 			song.tag.save()
-			
 			os.rename(pathsList[songName], folderPath+song.tag.artist+' - '+song.tag.title+'.mp3')
 			print("Updated '{0}'.".format(song.tag.artist+' - '+song.tag.title))
-
 		except TypeError:
 			print("No metadata found for '{0}'. Skipping it.".format(songName))
-
 		except AttributeError:
 			print("'{0}' is not an mp3 file. Skipping it.".format(songName))
 
