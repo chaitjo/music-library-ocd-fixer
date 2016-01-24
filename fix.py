dc45f9b479cf65207f0ec65e8f04a114d3e9eda9
Chaitanya Joshi <ckjoshi9@gmail.com>
2016-01-24 09:15:50 +0800

    Update fix.py

--- fix.py
+++ fix.py
@@ -14,7 +14,7 @@ def getData(name):
 	artist_name = urllib2.urlopen("http://rhythmsa.ga/api.php?api=artist_name&q={0}".format(name))
 	album_name = urllib2.urlopen("http://rhythmsa.ga/api.php?api=album_name&q={0}".format(name))
 	track_lyrics = getLyrics(artist_name, track_name)		#names should be properly formatted
-	return ( unicode(track_name.read(),'utf-8'), unicode(artist_name.read(),'utf-8'), unicode(album_name.read(),'utf-8') )
+	return ( unicode(track_name.read(),'utf-8'), unicode(artist_name.read(),'utf-8'), unicode(album_name.read(),'utf-8'), unicode(track_lyrics, 'utf-8') )
 
 def getLyrics(artist_name, track_name):
 	"""
